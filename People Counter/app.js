eb6b1348444cad44f6620211b717f03160bb7f46
Toufiq Ahmed <toufiq_a24@outlook.com>
2021-07-02 23:42:12 +0600

    people-counter-app

---
+++ People Counter/app.js
@@ -0,0 +1,24 @@
+
+//  initialize the count as 0
+
+//  listen for clicks on the increment button
+
+//  increment the count variable when the button is clicked
+
+//  change the count-el in the HTML to reflect the new count
+
+let countEl = document.getElementById("count-el")
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+
+//  create a function, save(), which logs out the count when it's called 
+
+function save() {
+    console.log(count)
+}
+
