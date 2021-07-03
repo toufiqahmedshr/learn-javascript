235747da8a9dd976a9a42d7297a70d0208d903b5
Toufiq Ahmed <toufiq_a24@outlook.com>
2021-07-04 00:23:33 +0600

    modified

---
+++ Blackjack Game/app.js
@@ -0,0 +1,60 @@
+
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function getRandomCard() {
+    let randomNum = Math.round(Math.random() * 13)
+    if (randomNum > 10) {
+        return 10
+    }
+    else if (randomNum === 1) {
+        return 11
+    }
+    else {
+        return randomNum
+    }
+}
+
+function startGame() {
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: " + sum
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    }
+    else if (sum === 21) {message = "Wohoo! You've got blackjack! 🥳"
+        hasBlackJack = true
+    }
+    else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+}
+
+function newCard() {
+    let card = getRandomCard()
+    cards.push(card)
+    sum += card
+    renderGame()
+}
